285938e5e3cc9f75fc54983154448dcbcda78b04
Ruth Joanna <ruthrachel0106@gmail.com>
2019-12-26 21:29:31 +0530

    complete setup of back-end

--- iuris/back-end/app.js
+++ iuris/back-end/app.js
@@ -2,29 +2,21 @@ const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-const expressValidator = require("express-validator");
+//const expressValidator = require("express-validator");
 const cors = require("cors");
-const cookieParser = require("cookie-parser");
+
 //config file
 dotenv.config();
 //starting the app
 const app = express();
 
-const authorizationError = function(err,req,res,next){
-    console.log(err.name);
-    if(err.name === 'UnauthorizedError'){
-       return res.status(401).json({error:"Unauthorized"});
-    }
-}
 
 //middleware
 app.use(morgan("dev"));
 app.use(bodyParser.json());
-app.use(expressValidator());
+//app.use(expressValidator());
 app.use(cors());
-app.use(cookieParser());
 
-const port =  process.env.PORT || 3000;m
+const port =  process.env.PORT || 3000;
 
 app.listen(port, ()=>{console.log(`Listening at port: ${port}`)});
-
