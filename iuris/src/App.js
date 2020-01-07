4ff7c7c2b2e1af30f2a9622c075636e85d154c00
aamerfaiz <aamrfz@gmail.com>
2020-01-07 23:18:10 +0530

    added front office homepage and add page

---
+++ iuris/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import MainRouter from './MainRouter'
+import {BrowserRouter} from 'react-router-dom'
+import "./App.css"
+
+const App = ()=>(
+  <BrowserRouter><MainRouter/></BrowserRouter>
+)
+
+
+export default App;cmd
