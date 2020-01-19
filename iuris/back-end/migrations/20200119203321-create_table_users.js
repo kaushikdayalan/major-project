cefdd42e55d521e2de55296f7c01a05667c86fcd
kaushikdayalan <kaushikruth1@gmail.com>
2020-01-20 02:12:28 +0530

    working with migrations

---
+++ iuris/back-end/migrations/20200119203321-create_table_users.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users',{
+      id:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    userName:{
+        type: Sequelize.STRING,
+        allowNull:false
+    },
+    password:{
+        type:Sequelize.STRING,
+        allowNull: false
+    },
+    createdAt:Sequelize.DATE,
+    updatedAt:Sequelize.DATE,
+    })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users')
+  }
+};
