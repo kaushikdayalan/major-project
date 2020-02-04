bcb58fb335dccd868ee48d4d929348c7b147898e
kaushikdayalan <kaushikruth1@gmail.com>
2020-02-04 20:58:25 +0530

    added state variables

--- iuris/front-end/src/components/core/AdminLogin.js
+++ iuris/front-end/src/components/core/AdminLogin.js
@@ -1,16 +1,30 @@
 import React, {Component} from "react"
 import { Card, CardDeck, CardTitle, Row, Col, Container, Input, Form, FormGroup, Button} from 'reactstrap';
-import {Link} from 'react-router-dom';
+import {Link, Redirect} from 'react-router-dom';
 import "../../../node_modules/bootstrap-material-design/dist/css/bootstrap-material-design.min.css";
 
 class AdminLogin extends Component{
   constructor(){
     super()
     this.state = {
-      
-    }
+      userName:"",
+      password:"",
+      error:"",
+      redirectTorefer:false,
+      loading:false
+    } 
   }
+
+  handleChange = name=>event=>{
+    this.setState({[name]: event.target.value}); 
+    this.setState({error:""});
+  };
+
     render(){
+      const {userName, password, error, redirectTorefer, loading} = this.state
+      if (redirectTorefer){
+        return <Redirect to="/frontoffice-home"></Redirect>
+      }
         return(
           <div className="right-pad-login">
             <Container>
@@ -22,7 +36,6 @@ class AdminLogin extends Component{
             className="text-center justify-content-center"
 						style={{ marginLeft: '1 px', float: 'right', width: '100%', height: '100%' }}
             >
-
               <Card
 							className="cards text-center"
 							body
@@ -38,16 +51,20 @@ class AdminLogin extends Component{
 
               <Form>
                 <FormGroup>
-                  <Input style={{color:'#bbb'}}
-                  type="email" placeholder="Enter username"/>
-                  <Input style={{color:'#bbb'}}
-                  type="password" placeholder="Enter password"/>
+                  <Input style={{color:'#bbb'}} onChange={this.handleChange("userName")}
+                  type="email" value={userName} placeholder="Enter username"/>
+                  <Input style={{color:'#bbb'}} onChange={this.handleChange("password")}
+                  type="password" value={password} placeholder="Enter password"/>
                 </FormGroup>
               </Form>
+              {loading ? <div className="jumbotron text-center"><h2>Loading...</h2></div>:""}
+              <div className="alert alert-danger" style={{display: error?"":"none"}}>
+                    {error}
+                </div>
               <Button style={{color:'white', marginLeft:'2px'}} onClick="">Signin</Button>
               <Link 
 									className="btn btn-lg btn-info "
-									style={{ marginLeft: '5px', backgroundcolor: '#888888', top: '9px' }}
+									style={{ marginLeft: '5px', backgroundcolor: '#888888',top: '9px'}}
 									to = "/"
 								>back</Link>
 						</Card>
