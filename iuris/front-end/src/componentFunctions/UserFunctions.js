378fea2a1d42a3e67034be8929d2fd448ca1f91b
Lamkhothang Vaiphei <Lam@MacBook-Pro.local>
2020-02-04 21:02:32 +0530

    Added signin userfunction

--- iuris/front-end/src/componentFunctions/UserFunctions.js
+++ iuris/front-end/src/componentFunctions/UserFunctions.js
@@ -1,1 +1,24 @@
-kjadhsakdaskj
+export const isAuthenticated =() =>{
+    if(typeof window=="undefined"){
+        return false;
+    }
+    if(localStorage.getItem("jwt")){
+        return JSON.parse(localStorage.getItem("jwt"));
+    }
+    else return false;
+}
+
+export const signin = user =>{
+    return fetch("http://localhost:8080/login",{
+        method:"POST",
+        headers:{
+            Accept:"application/json",
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify(user)
+    })
+    .then(response =>{
+        return response.json()
+    })
+    .catch(err => console.log(err))
+};
