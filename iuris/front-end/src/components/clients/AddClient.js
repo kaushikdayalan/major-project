a413b97a386d11d0a84d7dbc4954c2a595a6389a
Lamkhothang Vaiphei <Lam@MacBook-Pro-2.local>
2020-02-20 20:53:47 +0530

    Clean Start

--- iuris/front-end/src/components/clients/AddClient.js
+++ iuris/front-end/src/components/clients/AddClient.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react'
+import {Link} from 'react-router-dom'
 
 class AddClient extends Component{
     constructor(){
@@ -79,7 +80,8 @@ class AddClient extends Component{
                         <div className="alert alert-danger" style={{display: error?"":"none"}}>
                             {error}
                         </div>
-                    <button type="submit" class="btn btn-primary" onClick={this.clickSubmit}>Submit</button>
+                    <button type="submit" className="btn btn-raised btn-primary" style={{margin:"10px"}} onClick={this.clickSubmit}>Submit</button>
+                    <Link className="btn btn-raised btn-primary" to="/frontoffice-home" style={{margin:"10px"}}>back</Link>
                 </form>
                 </div>
                 <div className="alert alert-primary" style={{display: message?"":"none"}}>
