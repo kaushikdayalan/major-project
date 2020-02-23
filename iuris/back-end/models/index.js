62187f9e05c71a85a2d07fd39f5944f40e522e9d
Lamkhothang Vaiphei <Lam@MacBook-Pro-2.local>
2020-02-23 20:45:11 +0530

    added the back button on the FrontOfficeUpdate

--- iuris/back-end/models/index.js
+++ iuris/back-end/models/index.js
@@ -7,12 +7,13 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
-
+const timezone = 'Asia/Kolkata';
+const moment = require('moment').tz.setDefault(timezone);
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(config.database, config.username, config.password, config, timezone);
 }
 
 fs
